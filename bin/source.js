5c9a93e87aa14576ec7e48220a2a781fc4a5bc7c
Nanqiao Deng <nqdeng@gmail.com>
2014-05-20 16:52:29 +0800

    big change

--- bin/source.js
+++ bin/source.js
@@ -1,133 +1,26 @@
-var fs = require('./fs'),
+var fs = require('fs'),
+	path = require('path'),
 	util = require('../lib/util');
 
 var Source = util.inherit(Object, {
-		/**
-		 * Initializer.
-		 * @param config {Object}
-		 */
-		_initialize: function (config) {
-			this._patch = config.patch;
-			this._root = config.root;
-
-			this._map = fs.readDirectory(this._patch, '.', true);
+		_initialize: function (root) {
+			this._root = root;
 		},
 
-		/**
-		 * Determine file type.
-		 * @param pathname {string}
-		 * @return {string|null}
-		 */
-		_stat: function (pathname) {
+		travel: function (callback, pathname) {
+			pathname = pathname || '';
+
 			var root = this._root,
-				patch = this._patch,
-				map = this._map;
+				node = path.join(root, pathname),
+				stats = fs.statSync(node),
+				self = this;
 
-			if (map.hasOwnProperty(pathname)) {
-				return map[pathname] ?
-					'dir' : 'file';
-			} else {
-				return fs.stat(root, pathname);
-			}
-		},
-
-		/**
-		 * Clean patch directory.
-		 */
-		cleanup: function () {
-			fs.empty(this._patch, '.', true);
-		},
-
-		/**
-		 * Copy files in patch directory into source directory.
-		 */
-		merge: function () {
-			fs.mix(this._root, this._patch);
-		},
-
-		/**
-		 * Determine compiling range.
-		 * @param [deps] {Object}
-		 * @return {Array}
-		 */
-		range: function (deps) {
-			if (deps) {
-				var patchList = this._map,
-					range = [],
-					tmp = {};
-
-				// Reversed dependencies data.
-				util.each(deps, function (deps, pathname) {
-					deps.forEach(function (dep) {
-						if (!tmp[dep]) {
-							tmp[dep] = [ pathname ];
-						} else {
-							tmp[dep].push(pathname);
-						}
-					});
+			if (stats.isFile()) {
+				callback(pathname, fs.readFileSync(node));
+			} else if (stats.isDirectory()) {
+				fs.readdirSync(node).forEach(function (filename) {
+					self.travel(callback, path.join(pathname, filename));
 				});
-				deps = tmp;
-
-				// Determine affected files.
-				util.each(patchList, function (isDirectory, pathname) {
-					range = range.concat(
-						isDirectory ? [] : pathname,
-						deps[pathname] || []
-					);
-				});
-			} else {
-				var sourceList = fs.readDirectory(this._root, '.', true),
-					mixedList = util.mix(sourceList, this._map),
-					range = [];
-
-				util.each(mixedList, function (isDirectory, pathname) {
-					if (!isDirectory) {
-						range.push(pathname);
-					}
-				});
-			}
-
-			return range;
-		},
-
-		/**
-		 * Read a file or directory.
-		 * @param pathname {string}
-		 * @return {Object|null}
-		 */
-		read: function (pathname) {
-			var root = this._root,
-				patch = this._patch,
-				map = this._map;
-
-			switch (this._stat(pathname)) {
-			case 'file':
-				return {
-					pathname: pathname,
-					data: fs.readFile(map.hasOwnProperty(pathname) ?
-						patch : root, pathname)
-				};
-			case 'dir':
-				return {
-					pathname: pathname,
-					data: null
-				};
-			default:
-				return null;
-			}
-		},
-
-		/**
-		 * Write a file or make a directory.
-		 * @param file {Object}
-		 */
-		write: function (file) {
-			var root = this._root;
-
-			if (file.data) {
-				fs.writeFile(root, file.pathname, file.data);
-			} else {
-				fs.makeDirectory(root, file.pathname);
 			}
 		}
 	});
--- bin/target.js
+++ bin/target.js
@@ -1,59 +1,30 @@
-var fs = require('./fs'),
+var fs = require('fs'),
+	path = require('path'),
 	util = require('../lib/util');
 
-var Target = util.inherit(Object, {
-		/**
-		 * Initializer.
-		 * @param config {Object}
-		 */
-		_initialize: function (config) {
-			this._root = config.root;
+var	makeDirectory = function (root, pathname) {
+		pathname = path.dirname(pathname);
+
+		(function next(pathname) {
+			if (!fs.existsSync(pathname)) {
+				// At first, make parent directory.
+				next(path.resolve(pathname, '..'));
+				// Then, make current directory.
+				fs.mkdirSync(pathname);
+			}
+		}(path.join(root, pathname)));
+	},
+
+	Target = util.inherit(Object, {
+		_initialize: function (root) {
+			this._root = root;
 		},
 
-		/**
-		 * Clean target directory.
-		 */
-		cleanup: function () {
-			fs.empty(this._root, '.', false, [ '.meta' ]);
-		},
-
-		/**
-		 * Read a file or directory.
-		 * @param pathname {string}
-		 * @return {Object|null}
-		 */
-		read: function (pathname) {
+		write: function (pathname, data) {
 			var root = this._root;
 
-			switch (fs.stat(root, pathname)) {
-			case 'file':
-				return {
-					pathname: pathname,
-					data: fs.readFile(root, pathname)
-				};
-			case 'dir':
-				return {
-					pathname: patname,
-					data: null
-				};
-			default:
-				return null;
-			}
-		},
-
-		/**
-		 * Write a file or make a directory.
-		 * @param file {Object}
-		 * @return {Error|null}
-		 */
-		write: function (file) {
-			var root = this._root;
-
-			if (file.data === null) {
-				fs.makeDirectory(root, file.pathname);
-			} else {
-				fs.writeFile(root, file.pathname, file.data);
-			}
+			makeDirectory(root, pathname);
+			fs.writeFileSync(path.join(root, pathname), data);
 		}
 	});
 
