5e0ea9d36ddcb97c919874f01aaa8bdf40460a2f
Nanqiao Deng <nqdeng@gmail.com>
2014-11-14 14:48:11 +0800

    restore all to 2.0.12

--- bin/target.js
+++ bin/target.js
@@ -1,30 +1,69 @@
-var fs = require('fs'),
-	path = require('path'),
+var fs = require('./fs'),
 	util = require('../lib/util');
 
-var	makeDirectory = function (root, pathname) {
-		pathname = path.dirname(pathname);
-
-		(function next(pathname) {
-			if (!fs.existsSync(pathname)) {
-				// At first, make parent directory.
-				next(path.resolve(pathname, '..'));
-				// Then, make current directory.
-				fs.mkdirSync(pathname);
-			}
-		}(path.join(root, pathname)));
-	},
-
-	Target = util.inherit(Object, {
-		_initialize: function (root) {
-			this._root = root;
+var Target = util.inherit(Object, {
+		/**
+		 * Initializer.
+		 * @param config {Object}
+		 */
+		_initialize: function (config) {
+			this._root = config.root;
 		},
 
-		write: function (pathname, data) {
+		/**
+		 * Clean target directory.
+		 */
+		cleanup: function () {
+			fs.empty(this._root, '.', false, [ '.meta' ]);
+		},
+
+        /**
+         * remove file
+         * @param pathname
+         */
+        remove: function (pathname){
+            var root = this._root;
+
+            fs.removeFile(root, pathname);
+        },
+
+		/**
+		 * Read a file or directory.
+		 * @param pathname {string}
+		 * @return {Object|null}
+		 */
+		read: function (pathname) {
 			var root = this._root;
 
-			makeDirectory(root, pathname);
-			fs.writeFileSync(path.join(root, pathname), data);
+			switch (fs.stat(root, pathname)) {
+			case 'file':
+				return {
+					pathname: pathname,
+					data: fs.readFile(root, pathname)
+				};
+			case 'dir':
+				return {
+					pathname: patname,
+					data: null
+				};
+			default:
+				return null;
+			}
+		},
+
+		/**
+		 * Write a file or make a directory.
+		 * @param file {Object}
+		 * @return {Error|null}
+		 */
+		write: function (file) {
+			var root = this._root;
+
+			if (file.data === null) {
+				fs.makeDirectory(root, file.pathname);
+			} else {
+				fs.writeFile(root, file.pathname, file.data);
+			}
 		}
 	});
 
